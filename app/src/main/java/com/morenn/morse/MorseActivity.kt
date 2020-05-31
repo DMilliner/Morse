246f88771a4f6fa2049a8f03194079c57e2dd433
dmilliner <davidmilliner@Davids-MacBook-Pro.local>
2020-05-30 18:58:36 -0700

    Set up new layout

---
+++ app/src/main/java/com/morenn/morse/MorseActivity.kt
@@ -0,0 +1,12 @@
+package com.morenn.morse
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MorseActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_morse)
+    }
+}
